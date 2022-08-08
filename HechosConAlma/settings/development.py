81ddb03d4f99b23c3f15e72418941d8887384491
ezequieljm <ezequieljm020@gmail.com>
2022-08-08 16:26:50 -0300

    home page created

--- HechosConAlma/settings/development.py
+++ HechosConAlma/settings/development.py
@@ -1,5 +1,5 @@
 from .base import *
 
 
-ALLOWED_HOSTS = [ '*.herokuapp.com', 'dev-final-project-info.herokuapp.com', ]
+ALLOWED_HOSTS = [ 'localhost', '*.herokuapp.com', 'dev-final-project-info.herokuapp.com', ]
 

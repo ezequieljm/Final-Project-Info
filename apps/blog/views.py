a3e54eeb14bd0c4884a3aa5b9283d777542dc79c
ezequieljm <ezequieljm020@gmail.com>
2022-08-13 13:30:39 -0300

    Test 1 for deploy

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 # Create your views here.
 
 def home(request):
-    posts = Post.objects.all()
-  # return render(request, 'home/home.html', {'posts' : posts})
-    return render(request, 'home/home.html', {'posts' : posts})
+    # posts = Post.objects.all()
+#   return render(request, 'home/home.html', {'posts' : posts})
+    return render(request, 'home/home.html')

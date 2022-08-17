010d3173b64db7ac697cc52c1256b89873e0adc8
ezequieljm <ezequieljm020@gmail.com>
2022-08-16 21:36:12 -0300

    Anything

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Administrator, Member, Area, Post
+from .models import Administrator, Member, Area, Post 
+from .models import Person
 
 # Register your models here.
 
-# admin.site.register(Persons)
+admin.site.register(Person)
 admin.site.register(Member)
 admin.site.register(Administrator)
 admin.site.register(Area)

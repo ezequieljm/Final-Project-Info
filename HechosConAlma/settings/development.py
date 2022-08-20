4408645b229dd010fabcec2f1c6a552c77258ddd
ezequieljm <75186985+ezequieljm@users.noreply.github.com>
2022-08-19 21:21:56 -0300

    Update development.py

--- HechosConAlma/settings/development.py
+++ HechosConAlma/settings/development.py
@@ -1,5 +1,13 @@
 from .base import *
+import dj_database_url
+import os
 
-
+URL_POSTGRES = os.getenv("DATABASE_URL")
 ALLOWED_HOSTS = [ 'localhost', '*.herokuapp.com', 'dev-final-project-info.herokuapp.com', ]
+
+
+
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+DATABASES['default'] = dj_database_url.parse(URL_POSTGRES, conn_max_age=600)
 
